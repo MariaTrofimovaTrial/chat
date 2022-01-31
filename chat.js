df7936ec0ad4c4b6a6c2f95be05d9a695c6d045b
MARYIA TRAFIMAVA <psyenglish.school@gmail.com>
2022-01-31 21:19:31 +0300

    add greetings

--- chat.js
+++ chat.js
@@ -1,3 +1,4 @@
+const User = require('./user')
 const readline = require('readline');
 const rl = readline.createInterface({
    input: process.stdin,
@@ -5,7 +6,22 @@ const rl = readline.createInterface({
 });
 
 rl.question('Как тебя зовут? ', function (name) {
-   console.log(`Привет, ${name.toUpperCase()}`);
+   const user = new User();
+   user.greeting(name);
+   if (user.name != null) {
+      let end1 = 'ая';
+      let end2 = 'ая';
+      const userName = user.name.charAt(0).toUpperCase() + user.name.slice(1);
+      if (user.gender == 'male') {
+         end1 = 'ой';
+         end2 = 'ый';
+      }
+      console.log(`Привет, ${userName} Ты так${end1} красив${end2} сегодня!`);
+   }
+   // } else if (user.name != null && user.gender == 'female') {
+   //    const userName = user.name.charAt(0).toUpperCase() + user.name.slice(1);
+   //    console.log(`Привет, ${userName} Ты такая красивая сегодня!`);
+   // }
    rl.close();
 });
 
@@ -13,3 +29,4 @@ rl.question('Как тебя зовут? ', function (name) {
 rl.on('close', function () {
    process.exit(0);
 });
+
